0038d11561684c9ee258b3462483a0e827653b4d
vilmacio <vilmaciomoura@gmail.com>
2024-11-26 14:38:51 -0300

    update global loading page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -6,12 +6,9 @@ import type { } from '@mui/x-charts/themeAugmentation';
 import type { } from '@mui/x-data-grid/themeAugmentation';
 import type { } from '@mui/x-tree-view/themeAugmentation';
 import MainGrid from './components/MainGrid';
-import ProtectedPage from '../wrappers/protected-page';
 
 export default function Dashboard() {
   return (
-    <ProtectedPage>
-      <MainGrid />
-    </ProtectedPage>
+    <MainGrid />
   );
 }
--- src/app/wrappers/protected-page.tsx
+++ src/app/wrappers/protected-page.tsx
@@ -1,6 +1,6 @@
 'use client'
+import { CircularProgress, Stack } from "@mui/material";
 import { signIn, useSession } from "next-auth/react";
-import { useEffect } from "react";
 
 export default function ProtectedPage({ children }: { children: React.ReactNode, }) {
   const { status, data } = useSession({
@@ -10,15 +10,27 @@ export default function ProtectedPage({ children }: { children: React.ReactNode,
     },
   })
 
-  if (status === "loading") {
-    return <>loading...</>
+  if (status === 'loading') {
+    return (
+      <div className="flex justify-center items-center" style={{ height: '100vh' }}>
+        <Stack className="text-center" spacing={3}>
+          <div className="items-center">
+            <CircularProgress />
+          </div>
+          <Stack>
+            <h1 className='text-2xl'>Radiological Information System</h1>
+            <h2 className='text-sm opacity-75'>KAI Prevention Center</h2>
+          </Stack>
+        </Stack>
+      </div>
+    )
   }
 
   return (
     <div>
-      <div className="background-white" style={{ maxWidth: '300px', overflowX: 'scroll' }}>
+      {/* <div style={{ maxWidth: '300px', overflowX: 'scroll' }}>
         <pre>{JSON.stringify(data, null, 2)}</pre>
-      </div>
+      </div> */}
       {children}
     </div>
   )
