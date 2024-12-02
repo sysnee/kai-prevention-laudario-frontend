de016a010575df4b9f5c644a29ba3e0461bcfb08
Jean Carlos <jeancarllos99@outlook.com>
2024-12-02 12:55:32 -0300

    fix: corrigir apresentação da sidebar

--- src/app/dashboard/components/MenuContent.tsx
+++ src/app/dashboard/components/MenuContent.tsx
@@ -1,23 +1,22 @@
-import * as React from "react";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import Stack from "@mui/material/Stack";
-import Collapse from "@mui/material/Collapse";
-import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
-import PeopleRoundedIcon from "@mui/icons-material/PeopleRounded";
-import SettingsRoundedIcon from "@mui/icons-material/SettingsRounded";
-import InfoRoundedIcon from "@mui/icons-material/InfoRounded";
-import HelpRoundedIcon from "@mui/icons-material/HelpRounded";
-import ExpandLess from "@mui/icons-material/ExpandLess";
-import ExpandMore from "@mui/icons-material/ExpandMore";
-import DocumentScannerRounded from "@mui/icons-material/DocumentScannerRounded";
-import { CalendarIcon } from "@mui/x-date-pickers";
-import { MonitorHeart, SecurityOutlined } from "@mui/icons-material";
-import { Divider } from "@mui/material";
-import Link from "next/link";
+import * as React from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Stack from '@mui/material/Stack';
+import Collapse from '@mui/material/Collapse';
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import PeopleRoundedIcon from '@mui/icons-material/PeopleRounded';
+import SettingsRoundedIcon from '@mui/icons-material/SettingsRounded';
+import ExpandLess from '@mui/icons-material/ExpandLess';
+import ExpandMore from '@mui/icons-material/ExpandMore';
+import DocumentScannerRounded from '@mui/icons-material/DocumentScannerRounded';
+import { CalendarIcon } from '@mui/x-date-pickers';
+import { MonitorHeart, SecurityOutlined } from '@mui/icons-material';
+import { Divider } from '@mui/material';
+import Link from 'next/link';
+import { User2Icon } from 'lucide-react';
 
 const mainListItems = [
   {
@@ -51,13 +50,8 @@ const mainListItems = [
 ];
 
 const secondaryListItems = [
-  {
-    text: "Permissões",
-    icon: <SettingsRoundedIcon />,
-    path: "/dashboard/permissions",
-  },
-  { text: "Sobre", icon: <InfoRoundedIcon /> },
-  { text: "Feedback", icon: <HelpRoundedIcon /> },
+  { text: 'Usuários', icon: <User2Icon />, path: '/dashboard/usuarios' },
+  { text: 'Permissões', icon: <SettingsRoundedIcon />, path: '/dashboard/permissions' },
 ];
 
 export default function MenuContent() {
@@ -71,7 +65,7 @@ export default function MenuContent() {
   };
 
   return (
-    <Stack sx={{ flexGrow: 1, p: 1, justifyContent: "space-between" }}>
+    <Stack sx={{ flexGrow: 1, p: 1 }} spacing={4}>
       <List dense>
         {mainListItems.map((item, index) => (
           <React.Fragment key={index}>
@@ -114,29 +108,27 @@ export default function MenuContent() {
       </List>
 
       <Divider />
-      <Stack direction={"row"} spacing={1} sx={{ p: 1 }}>
-        <SecurityOutlined className="opacity-50" style={{ width: "20px" }} />
-        <p className="opacity-75">ADMINISTRATIVO</p>
-      </Stack>
-      <List dense>
-        {secondaryListItems.map((item, index) => (
-          <ListItem key={index} disablePadding sx={{ display: "block" }}>
-            {item.path ? (
-              <Link href={item.path} passHref>
+
+      <Stack>
+        <Stack direction={'row'} spacing={1}>
+          <SecurityOutlined className='opacity-50' style={{ width: '20px' }} />
+          <p className='opacity-75'>ADMINISTRATIVO</p>
+        </Stack>
+        <List dense>
+          {secondaryListItems.map((item, index) => (
+            <Link key={index} href={item.path}>
+              <ListItem key={index} disablePadding sx={{ display: 'block' }}>
                 <ListItemButton>
                   <ListItemIcon>{item.icon}</ListItemIcon>
                   <ListItemText primary={item.text} />
                 </ListItemButton>
-              </Link>
-            ) : (
-              <ListItemButton>
-                <ListItemIcon>{item.icon}</ListItemIcon>
-                <ListItemText primary={item.text} />
-              </ListItemButton>
-            )}
-          </ListItem>
-        ))}
-      </List>
+              </ListItem>
+            </Link>
+          ))}
+        </List>
+      </Stack>
+
+
     </Stack>
   );
 }
