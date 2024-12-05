10e90a2d8fbcac3df35a212ee9cd2f6f9eea6479
Jean Carlos <jeancarllos99@outlook.com>
2024-12-04 23:54:01 -0300

    feat: remover console.log desnecessário

--- src/app/dashboard/permissions/components/PermissionsList.tsx
+++ src/app/dashboard/permissions/components/PermissionsList.tsx
@@ -61,151 +61,141 @@ export default function PermissionsList({
           : "0 2px 8px rgba(0,0,0,0.1)",
       }}
     >
-      {rolesPermissions.map(
-        (role, index) => (
-          console.log(
-            role.permissions.find((p) => p.module == "client")?.access
-          ),
-          (
-            <div key={role.name}>
-              <ListItem
-                sx={{
-                  display: "flex",
-                  flexDirection: { xs: "column", sm: "row" },
-                  alignItems: { xs: "flex-start", sm: "center" },
-                  justifyContent: "space-between",
-                  "&:hover": {
-                    backgroundColor: isDarkMode
-                      ? theme.palette.action.hover
-                      : "rgba(0, 0, 0, 0.04)",
-                  },
-                }}
-              >
-                <Box
-                  sx={{
-                    flex: 1,
-                    display: "flex",
-                    flexDirection: "column",
-                    gap: 1,
-                    alignItems: "flex-start",
-                  }}
-                >
-                  <ListItemText
-                    primary={
-                      <Box
-                        sx={{ display: "flex", alignItems: "center", gap: 2 }}
-                      >
-                        <Typography
-                          component={"span"}
-                          sx={{
-                            color: theme.palette.text.primary,
-                            fontWeight: "bold !important",
-                          }}
-                        >
-                          {role.name}
-                        </Typography>
-                        <StatusChip isActive={role.isActive} />
-                      </Box>
-                    }
-                    secondary={
-                      <Box
-                        sx={{
-                          display: "flex",
-                          flexDirection: { xs: "column", sm: "row" },
-                          alignItems: { xs: "flex-start", sm: "center" },
-                          gap: 1,
-                          marginTop: 1,
-                        }}
-                      >
-                        <Typography
-                          component="span"
-                          sx={{
-                            fontSize: "0.875rem",
-                            color: theme.palette.text.secondary,
-                            fontWeight: 500,
-                          }}
-                        >
-                          Módulo de Clientes
-                        </Typography>
-                        <Box>
-                          <AccessChip
-                            access={
-                              role.permissions.find((p) => p.module == "client")
-                                ?.access || "none"
-                            }
-                            description={getPatientAccessDescription(
-                              role.permissions.find(
-                                (p) => p.module === "client"
-                              )?.access || "none",
-                              role.name!
-                            )}
-                          />
-                        </Box>
-                      </Box>
-                    }
-                  />
-                </Box>
-
-                <Box
-                  sx={{
-                    display: "flex",
-                    flexDirection: "row",
-                    justifyContent: "flex-end",
-                    alignItems: "center",
-                    gap: 1,
-                    width: "auto",
-                    marginTop: { xs: 2, sm: 0 },
-                  }}
-                >
-                  <ActionButtons
-                    onView={() => onView(role)}
-                    onEdit={() => onEdit(role)}
-                    onDelete={() => onDelete(role)}
-                  />
-                  <IconButton
-                    edge="end"
-                    onClick={() => handleToggle(role.name!)}
-                    size="small"
+      {rolesPermissions.map((role, index) => (
+        <div key={role.name}>
+          <ListItem
+            sx={{
+              display: "flex",
+              flexDirection: { xs: "column", sm: "row" },
+              alignItems: { xs: "flex-start", sm: "center" },
+              justifyContent: "space-between",
+              "&:hover": {
+                backgroundColor: isDarkMode
+                  ? theme.palette.action.hover
+                  : "rgba(0, 0, 0, 0.04)",
+              },
+            }}
+          >
+            <Box
+              sx={{
+                flex: 1,
+                display: "flex",
+                flexDirection: "column",
+                gap: 1,
+                alignItems: "flex-start",
+              }}
+            >
+              <ListItemText
+                primary={
+                  <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+                    <Typography
+                      component={"span"}
+                      sx={{
+                        color: theme.palette.text.primary,
+                        fontWeight: "bold !important",
+                      }}
+                    >
+                      {role.name}
+                    </Typography>
+                    <StatusChip isActive={role.isActive} />
+                  </Box>
+                }
+                secondary={
+                  <Box
+                    sx={{
+                      display: "flex",
+                      flexDirection: { xs: "column", sm: "row" },
+                      alignItems: { xs: "flex-start", sm: "center" },
+                      gap: 1,
+                      marginTop: 1,
+                    }}
                   >
-                    {expandedItems.includes(role.name!) ? (
-                      <ChevronDown size={18} />
-                    ) : (
-                      <ChevronRight size={18} />
-                    )}
-                  </IconButton>
-                </Box>
-              </ListItem>
-              <Collapse
-                in={expandedItems.includes(role.name!)}
-                timeout="auto"
-                unmountOnExit
+                    <Typography
+                      component="span"
+                      sx={{
+                        fontSize: "0.875rem",
+                        color: theme.palette.text.secondary,
+                        fontWeight: 500,
+                      }}
+                    >
+                      Módulo de Clientes
+                    </Typography>
+                    <Box>
+                      <AccessChip
+                        access={
+                          role.permissions.find((p) => p.module == "client")
+                            ?.access || "none"
+                        }
+                        description={getPatientAccessDescription(
+                          role.permissions.find((p) => p.module === "client")
+                            ?.access || "none",
+                          role.name!
+                        )}
+                      />
+                    </Box>
+                  </Box>
+                }
+              />
+            </Box>
+
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "flex-end",
+                alignItems: "center",
+                gap: 1,
+                width: "auto",
+                marginTop: { xs: 2, sm: 0 },
+              }}
+            >
+              <ActionButtons
+                onView={() => onView(role)}
+                onEdit={() => onEdit(role)}
+                onDelete={() => onDelete(role)}
+              />
+              <IconButton
+                edge="end"
+                onClick={() => handleToggle(role.name!)}
+                size="small"
               >
-                <Box
-                  sx={{
-                    padding: "16px",
-                    backgroundColor: isDarkMode
-                      ? theme.palette.background.paper
-                      : "rgba(0,0,0,0.02)",
-                    borderRadius: "8px",
-                    margin: "8px 16px",
-                  }}
-                >
-                  <StagePermissions stages={role.examStages} />
-                </Box>
-              </Collapse>
-              {index < rolesPermissions.length - 1 && (
-                <Divider
-                  sx={{
-                    backgroundColor: isDarkMode
-                      ? theme.palette.divider
-                      : "rgba(0, 0, 0, 0.1)",
-                  }}
-                />
-              )}
-            </div>
-          )
-        )
-      )}
+                {expandedItems.includes(role.name!) ? (
+                  <ChevronDown size={18} />
+                ) : (
+                  <ChevronRight size={18} />
+                )}
+              </IconButton>
+            </Box>
+          </ListItem>
+          <Collapse
+            in={expandedItems.includes(role.name!)}
+            timeout="auto"
+            unmountOnExit
+          >
+            <Box
+              sx={{
+                padding: "16px",
+                backgroundColor: isDarkMode
+                  ? theme.palette.background.paper
+                  : "rgba(0,0,0,0.02)",
+                borderRadius: "8px",
+                margin: "8px 16px",
+              }}
+            >
+              <StagePermissions stages={role.examStages} />
+            </Box>
+          </Collapse>
+          {index < rolesPermissions.length - 1 && (
+            <Divider
+              sx={{
+                backgroundColor: isDarkMode
+                  ? theme.palette.divider
+                  : "rgba(0, 0, 0, 0.1)",
+              }}
+            />
+          )}
+        </div>
+      ))}
     </List>
   );
 }
